558890568ea4a9fcf32f07d651e73d72a25dfe75
student <gamebot@bluewiz.net>
2021-05-04 12:03:59 -0700

    fix

--- src/com/mrjaffesclass/othello/Othello.java
+++ src/com/mrjaffesclass/othello/Othello.java
@@ -18,7 +18,7 @@ public class Othello {
   public static void main(String[] args) {
     Controller c = new Controller( 
       new SmartPlayer(Constants.BLACK), 
-      new StrategyPlayer(Constants.WHITE)
+      new OldChamp(Constants.WHITE)
     );
     c.displayMatchup();
     int result = c.run();
